a2886ec052312ca607e3b6c0a462ce37b44f2615
sumit yadav <ahsiray79@gmail.com>
2024-10-18 18:59:50 +0530

    compents and layouts added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
+import { Route, Routes } from "react-router-dom";
+import { About } from "./Components/About";
+import { Contact } from "./Components/Contact";
+import { Home } from "./Components/Home";
+import { Projects } from "./Components/Projects";
+import { Skills } from "./Components/Skills";
+
 function App() {
-  return <>Hello World</>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/skills" element={<Skills />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
